c253213661dc38c6a4db74265e0caca98b509045
AndreiBalan-dev <jenialulegion0@gmail.com>
2024-07-27 15:30:28 +0300

    login page modifications

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,10 +1,10 @@
-import { Form } from "react-router-dom"
+import { Form } from "react-router-dom";
 
 // library
 import { UserPlusIcon } from "@heroicons/react/24/solid";
 
 // assets
-import illustration from "../assets/illustration.jpg"
+import illustration from "../assets/illustration.jpg";
 
 const Intro = () => {
   return (
@@ -14,17 +14,23 @@ const Intro = () => {
           Take Control of <span className="accent">Your Money</span>
         </h1>
         <p>
-          Personal budgeting is the secret to financial freedom. Start your journey today.
+          Personal budgeting is the secret to financial freedom. Start your
+          journey today.
         </p>
         <Form method="post">
           <input
             type="text"
             name="userName"
             required
-            placeholder="What is your name?" aria-label="Your Name" autoComplete="given-name"
+            placeholder="What is your name?"
+            aria-label="Your Name"
+            autoComplete="given-name"
           />
           <input type="hidden" name="_action" value="newUser" />
-          <button type="submit" className="btn btn--dark bg-hslblack text-white flex items-center justify-center px-4 py-2 text-base font-normal mr-2">
+          <button
+            type="submit"
+            className="btn btn--dark bg-hslblack text-white flex items-center justify-center px-4 py-2 text-base font-normal mr-2"
+          >
             <span>Create Account</span>
             <UserPlusIcon width={20} />
           </button>
@@ -32,6 +38,6 @@ const Intro = () => {
       </div>
       <img src={illustration} alt="Person with money" width={600} />
     </div>
-  )
-}
-export default Intro
+  );
+};
+export default Intro;
