29cc6db62710de0bf291066087b117aefe720388
AndreiBalan-dev <jenialulegion0@gmail.com>
2024-07-28 02:44:05 +0300

    prettier changes

--- src/components/AddExpenseForm.jsx
+++ src/components/AddExpenseForm.jsx
@@ -1,41 +1,38 @@
 // react imports
-import { useEffect, useRef } from "react"
+import { useEffect, useRef } from "react";
 
 // rrd imports
-import { useFetcher } from "react-router-dom"
+import { useFetcher } from "react-router-dom";
 
 // library imports
-import { PlusCircleIcon } from "@heroicons/react/24/solid"
+import { PlusCircleIcon } from "@heroicons/react/24/solid";
 
 const AddExpenseForm = ({ budgets }) => {
-  const fetcher = useFetcher()
+  const fetcher = useFetcher();
   const isSubmitting = fetcher.state === "submitting";
 
-  const formRef = useRef()
-  const focusRef = useRef()
+  const formRef = useRef();
+  const focusRef = useRef();
 
   useEffect(() => {
     if (!isSubmitting) {
       // clear form
-      formRef.current.reset()
+      formRef.current.reset();
       // reset focus
-      focusRef.current.focus()
+      focusRef.current.focus();
     }
-
-  }, [isSubmitting])
+  }, [isSubmitting]);
 
   return (
     <div className="form-wrapper">
-      <h2 className="h3">Add New{" "}<span className="accent">
-        {budgets.length === 1 && `${budgets.map((budg) => budg.name)}`}
-      </span>{" "}
+      <h2 className="h3">
+        Add New{" "}
+        <span className="accent">
+          {budgets.length === 1 && `${budgets.map((budg) => budg.name)}`}
+        </span>{" "}
         Expense
       </h2>
-      <fetcher.Form
-        method="post"
-        className="grid-sm"
-        ref={formRef}
-      >
+      <fetcher.Form method="post" className="grid-sm" ref={formRef}>
         <div className="expense-inputs">
           <div className="grid-xs">
             <label htmlFor="newExpense">Expense Name</label>
@@ -64,32 +61,34 @@ const AddExpenseForm = ({ budgets }) => {
         <div className="grid-xs" hidden={budgets.length === 1}>
           <label htmlFor="newExpenseBudget">Budget Category</label>
           <select name="newExpenseBudget" id="newExpenseBudget" required>
-            {
-              budgets
-                .sort((a, b) => a.createdAt - b.createdAt)
-                .map((budget) => {
-                  return (
-                    <option key={budget.id} value={budget.id}>
-                      {budget.name}
-                    </option>
-                  )
-                })
-            }
+            {budgets
+              .sort((a, b) => a.createdAt - b.createdAt)
+              .map((budget) => {
+                return (
+                  <option key={budget.id} value={budget.id}>
+                    {budget.name}
+                  </option>
+                );
+              })}
           </select>
         </div>
         <input type="hidden" name="_action" value="createExpense" />
-        <button type="submit" className="btn btn--dark text-white bg-hslblack flex items-center justify-center px-4 py-2 text-base font-normal mr-2" disabled={isSubmitting}>
-          {
-            isSubmitting ? <span>Submitting…</span> : (
-              <>
-                <span>Add Expense</span>
-                <PlusCircleIcon width={20} />
-              </>
-            )
-          }
+        <button
+          type="submit"
+          className="btn btn--dark text-white bg-hslblack flex items-center justify-center px-4 py-2 text-base font-normal mr-2"
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? (
+            <span>Submitting…</span>
+          ) : (
+            <>
+              <span>Add Expense</span>
+              <PlusCircleIcon width={20} />
+            </>
+          )}
         </button>
       </fetcher.Form>
     </div>
-  )
-}
-export default AddExpenseForm
+  );
+};
+export default AddExpenseForm;
