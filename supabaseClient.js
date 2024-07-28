9117828835112ceaa0b64c2cbd1edb6257af8fc4
AndreiBalan-dev <jenialulegion0@gmail.com>
2024-07-28 11:06:38 +0300

    setup

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,10 +1,8 @@
-// src/supabaseClient.js
-
 import { createClient } from "@supabase/supabase-js";
 
 // Keys
-const supabaseUrl = "https://bxqljlahpxgvjybwodkl.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4cWxqbGFocHhndmp5YndvZGtsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIwNTI1MDYsImV4cCI6MjAzNzYyODUwNn0.eKcTCZ5k0cZRta4pBmx8o3qPd7tgW0YJOEzkAWQfq90";
+const supabaseUrl = import.meta.env.VITE_API_URL;
+const supabaseKey = import.meta.env.VITE_API_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
