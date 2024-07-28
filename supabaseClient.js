a5284b81764ea37b87e58d05db07c4c732aa356d
AndreiBalan-dev <jenialulegion0@gmail.com>
2024-07-28 11:34:57 +0300

    vercel setup fixes

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Keys
+
 const supabaseUrl = import.meta.env.VITE_API_URL;
 const supabaseKey = import.meta.env.VITE_API_KEY;
 
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    "process.env": process.env,
+    "import.meta.env.VITE_API_URL": JSON.stringify(process.env.VITE_API_URL),
+    "import.meta.env.VITE_API_KEY": JSON.stringify(process.env.VITE_API_KEY),
+  },
+});
