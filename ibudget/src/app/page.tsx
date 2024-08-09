d759f89489cac42505e110a8f51d629fa6fa0274
AndreiBalan-dev <jenialulegion0@gmail.com>
2024-08-10 00:04:13 +0300

    remove generated code to start rewriting

--- ibudget/src/app/page.tsx
+++ ibudget/src/app/page.tsx
@@ -1,20 +1,5 @@
-import Dashboard from "../components/Dashboard";
-import { dashboardLoader } from "../utils/helpers";
-import { DashboardProps } from "../types";
-
 const Home = async () => {
-  const { userName, budgets, expenses, waitlist } = await dashboardLoader();
-
-  return (
-    <div>
-      <Dashboard
-        userName={userName}
-        budgets={budgets}
-        expenses={expenses}
-        waitlist={waitlist}
-      />
-    </div>
-  );
+  return <div>Test</div>;
 };
 
 export default Home;
